feb37fc71601242a4153cf5caff5b8d9401f8edd
angel zambrano <angeldzambrano99@gmail.com>
2023-05-03 11:42:12 -0700

    removed storyboards

--- LoveYouLatte/LoveYouLatte/ViewController.swift
+++ LoveYouLatte/LoveYouLatte/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableview: UITableView = {
+       let tb = UITableView()
+        tb.backgroundColor = .systemGray6
+        tb.translatesAutoresizingMaskIntoConstraints = false // enabling programic autolayout
+        return tb
+        
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "edit"
+        setUpLayout()
+        
     }
-
+    
+    // MARK: setting up
+    
+    // setting up constraints and the views
+    private func setUpLayout() {
+        view.addSubview(tableview)
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    
+    }
 
 }
 
